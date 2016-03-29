ecd34ee7cb70d74ae941ecb9f3d2bd663496aab3
mobin <996833461@qq.com>
2016-03-29 21:28:47 +0800

    插入排序

--- src/com/mobin/sort/InsertSort.java
+++ src/com/mobin/sort/InsertSort.java
@@ -10,7 +10,7 @@ public class InsertSort {
         int len = arr.length;
         for(int i = 1; i < len; i ++) {
             if (arr[i] < arr[i-1]) {///注意[0,i-1]都是有序的。如果待插入元素比arr[i-1]还大则无需再与[i-1]前面的元素进行比较了，反之则进入if语句
-                for (int j = i; j > 0 && arr[j] < arr[j - 1]; j--) {
+                for (int j = i; j > 0  && arr[j] < arr[j - 1]; j--) {
                     int temp = arr[j];
                     arr[j] = arr[j - 1];   //把比arr[j]大的元素后移
                     arr[j - 1] = temp;  //把待插入的元素插入腾出的位置
